81e2d855079a13ab52c14757beca3cac1b6d8d4b
Lucia Saederup <luciasaederup@gmail.com>
2021-12-29 17:32:54 -0300

    Agregado de funcion keydown para el buscador

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-fetch("http(s)://gateway.marvel.com/")
-    .then((res) => {
+// fetch("http(s)://gateway.marvel.com/")
+//     .then((res) => {
 
-        return res.json()
-    })
-    .then((data) => {
-        console.log(data)
+//         return res.json()
+//     })
+//     .then((data) => {
+//         console.log(data)
 
-    })
+//     })
 
+const inputBuscar = document.getElementById('input-buscador');
 
-// "scss": "node-sass --watch style/scss -o style/css"
+inputBuscar.onkeydown = (e) => {
+    console.log(inputBuscar.value)
+}
+const busqueda = inputBuscar.value
+
+//  const buscarComics = (busqueda) => {
+//      fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${busqueda}`)
+//          .then(res => res.json())
+//          .then(data => {
+//             hacerHTMLTarjetas(data.results)
+//          })
+//  }
