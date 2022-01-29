546252e64f5521488738a574cd6292bd95563d18
Macarena Nava <nava.macarena@gmail.com>
2022-01-29 04:37:50 -0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-fetch("http(s)://gateway.marvel.com/")
-    .then((res) => {
 
-        return res.json()
-    })
-    .then((data) => {
-        console.log(data)
-
-    })
 
 
 // "scss": "node-sass --watch style/scss -o style/css"
