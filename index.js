8b45bb3c543a6c17384c1040f809fcdabdf98488
betty <betty.182.186@gmail.com>
2022-01-29 17:24:52 -0300

    agrego info

--- index.js
+++ index.js
@@ -10,3 +10,109 @@ fetch("http(s)://gateway.marvel.com/")
 
 
 // "scss": "node-sass --watch style/scss -o style/css"
+
+
+// open api  key marvel:93c0e369ba23b10fe80edb027c368e12
+
+fetch (`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=93c0e369ba23b10fe80edb027c368e12`)
+.then (res=> res.json())
+.then(data => console.log(data))
+
+let comicGlobal = 0
+
+
+/*
+character 018
+ver form y select de html 0123*/
+const listaComics = () => {
+fetch (`https://gateway.marvel.com:443/v1/public/comics?offset=${comicGlobal}&apikey=93c0e369ba23b10fe80edb027c368e12`)
+.then((res) => res.json())
+.then((data) => {
+    console.log(data.data.results)
+  crearTarjetasComics(data.data.results)
+     })
+ }
+
+ listaComics()
+
+ const crearTarjetasComics = (data) => {
+    const personajes = document.querySelector("#personajes")
+    const html = data.reduce((acc, curr) => {
+         return acc + `
+         <article class="personaje">
+         <h2>${curr.title}</h2>
+         <img src="${curr.thumbnail.path}.${curr.thumbnail.extension}">
+         </article>`
+         }, "")
+    personajes.innerHTML = html
+ }
+  
+
+
+
+const prev = document.querySelector("#prev")
+const next = document.querySelector("#next")
+
+
+if (comicGlobal === 0) {
+    prev.disabled = true
+}
+
+
+ next.onclick = () => {
+     
+    comicGlobal += 20
+    listaComics()
+    prev.disabled = false
+    
+ }
+
+ prev.onclick = () => {
+  
+    if (comicGlobal === 20) {
+        prev.disabled = true
+    }
+
+   
+    comicGlobal -= 20
+    listaComics()
+  
+ }
+
+
+
+
+ 
+personajesASaltar = 0
+
+ 
+ const listaPersonajes = () => {
+    fetch (`https://gateway.marvel.com:443/v1/public/characters?offset=${personajesASaltar}&apikey=93c0e369ba23b10fe80edb027c368e12`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data.data.results)
+      crearTarjetasComics(data.data.results)
+         })
+     }
+    
+     listaComics()
+
+     const crearTarjetasPersonaje = (data) => {
+        const personajes = document.querySelector("#personajes")
+        const html = data.reduce((acc, curr) => {
+             return acc + `
+             <article class="personaje">
+             <h2>${curr.name}</h2>
+             <img src="${curr.thumbnail.path}.${curr.thumbnail.extension}">
+             </article>`
+             }, "")
+        personajes.innerHTML = html
+     }
+     
+
+
+
+
+
+
+
