eb66f1e3f74a57b0fd6ccca809d4dc368faac3cf
Macarena Nava <nava.macarena@gmail.com>
2022-02-05 23:17:38 -0300

    url y apikey js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
-
-
-// "scss": "node-sass --watch style/scss -o style/css"
+//URL//
+const marvelUrl = "https://gateway.marvel.com/";
+const apiKey = "423b341c7eb99f57e1ff80044ac07c00";
+const hash = "1cc015d0c7a2767420a5d9c32e8d9a10";
