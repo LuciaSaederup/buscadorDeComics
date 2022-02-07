18f0595ff43e2738368d0f690b32a5c39d89c166
Macarena Nava <nava.macarena@gmail.com>
2022-02-06 21:44:17 -0300

    cambios y arreglos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //URL//
-const marvelUrl = "https://gateway.marvel.com/";
+const marvelUrl = "https://gateway.marvel.com/v1/public/";
 const apiKey = "423b341c7eb99f57e1ff80044ac07c00";
 const hash = "1cc015d0c7a2767420a5d9c32e8d9a10";
 
