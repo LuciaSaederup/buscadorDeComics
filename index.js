dca8dc5bfed70d68247dea5c1b2590a9dd1c7c6d
Macarena Nava <nava.macarena@gmail.com>
2022-02-06 23:34:08 -0300

    solucion de errores

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const marvelUrl = "https://gateway.marvel.com/v1/public/";
 const apiKey = "423b341c7eb99f57e1ff80044ac07c00";
 const hash = "1cc015d0c7a2767420a5d9c32e8d9a10";
 
-const cardsContainer= document.getElementById("cardsContainer")
+const cardsContainer = document.getElementById("cardsContainer");
 let totalPages;
 
 const updatingResultsCount = count =>{
     const cardsSinResultados = document.getElementById("cardsSinResultados");
     cardsSinResultados.innerHTML = count + " RESULTADOS";
-}
+};
 
 const createCards =  (response, type)=> {
     cardsContainer.innerHTML = "";
@@ -17,16 +17,16 @@ const createCards =  (response, type)=> {
     updatingResultsCount(total);
     const data = response.data.results;
     console.log(response);
-    
+
     data.forEach((element) => {
-        
+    
         const a = document.createElement("a");
         const card = document.createElement("div");
         const img = document.createElement("img");
         const title = document.createElement("h3");
-    
+
         a.setAttribute("href", "information.html?id=" + element.id + "&type=" + type + "&page=1");
-    
+
         if (element.thumbnail.path === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available") {
             img.setAttribute("src","imagenotfoundhydra.jpg");
         }
@@ -34,17 +34,18 @@ const createCards =  (response, type)=> {
             img.setAttribute("src", element.thumbnail.path + "." + element.thumbnail.extension);
         }
         const titleTxt = document.createTextNode(element.title || element.name);
-            img.classList.add("card-img");
-            title.classList.add("card-h3");
-            card.classList.add("card");
-            a.classList.add("links");
-            a.appendChild(img);
-            title.appendChild(titleTxt);
-            a.appendChild(title);
-            card.appendChild(a);
-            cardsContainer.appendChild(card);
-        });
+        img.classList.add("card-img");
+        title.classList.add("card-h3");
+        card.classList.add("card");
+        a.classList.add("links");
+        a.appendChild(img);
+        title.appendChild(titleTxt);
+        a.appendChild(title);
+        card.appendChild(a);
+        cardsContainer.appendChild(card);
+    });
 };   
+
 
 const getComics= async (developerPass) => {
     let data = [];
@@ -184,6 +185,7 @@ lastPage.addEventListener("click", () =>{
         });
     });
 });
+
 
 const cardsResultados = document.getElementById('cardsResultados');
 const cardInfo = document.getElementById('card-info');
@@ -332,6 +334,25 @@ const createCardInfo = async () => {
 };
 createCardInfo();
 
+
+
+const selType = document.getElementById("type");
+const inpSearch = document.getElementById("search");
+const orderComicsBy = document.getElementById("orderComicsBy");
+const orderCharactersBy = document.getElementById('orderCharactersBy');
+
+selType.addEventListener('change', () => {
+    if (selType.value === "characters") {
+        orderComicsBy.classList.add('d-none');
+        orderCharactersBy.classList.remove('d-none');
+    }
+    else {
+        orderComicsBy.classList.remove('d-none');
+        orderCharactersBy.classList.add('d-none');
+    }
+});
+
+
 const formSearch = document.getElementsByClassName("searchContainer")[0];
 const search = (event) => {
     event.preventDefault();
@@ -390,10 +411,6 @@ if(window.location.search === ""){
 } else {
     filtros(offset, searchTxt, type, orderBy);
 }
-
-
-
-
 
 
 
